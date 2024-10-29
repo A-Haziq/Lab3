da6b1f8913a7f3f2f6fcf8617d33abb4c9a6ba64
Haziq <amirulhaziqba08.23@ichat.sp.edu.sg>
2024-10-29 15:45:04 +0800

    Lab3 Exercise 1

---
+++ Test_bmi.py
@@ -0,0 +1,24 @@
+import Lab2.bmi as bmi
+
+print ("Lab3_Ex1")
+
+def test_bmi_normal_weight():
+    weight = 70
+    height = 1.75
+    result = bmi.calculate_bmi(height, weight)
+
+    assert result == 0
+
+def test_bmi_over_weight():
+    weight = 90
+    height = 1.75
+    result = bmi.calculate_bmi(height, weight)
+
+    assert result == 1
+
+def test_bmi_under_weight():
+    weight = 50
+    height = 1.8
+    result = bmi.calculate_bmi(height, weight)
+
+    assert result == -1
